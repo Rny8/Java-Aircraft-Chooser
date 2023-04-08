dfe565948911b8a90d2610af3c4cc3378f8f21fc
Rny8 <neongirl3@yahoo.com>
2023-04-07 23:11:22 -0500

    Added new function that automatiadclly asks questions related to the building of a new plane (prevent repetive code) Added feature to build plane

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
+
+    static Scanner scanner;
+    static PlaneInventory inventory = new PlaneInventory(new Plane[]{
+            new Plane("Cessno 142", 50, 1, 90),
+            new Plane("Cessno 146", 52, 1, 95),
+            new Plane("Cessno 148", 250, 2, 250)
+    });
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        PlaneInventory inventory = new PlaneInventory(new Plane[]{
-                new Plane("Cessno 142", 50, 1, 90),
-                new Plane("Cessno 146", 52, 1, 95),
-                new Plane("Cessno 148", 250, 1, 250)
-        });
+
+        scanner = new Scanner(System.in);
+
         boolean isInputting = true;
         while (isInputting) {
             System.out.print("How many miles would you like to fly, buddster boi: ");
@@ -17,10 +21,31 @@ public class Main {
             int userTypeRequest = Integer.valueOf(scanner.nextLine());
             Plane[] possiblePlanes = inventory.searchPlane(userMileRequest, userTypeRequest);
             if (possiblePlanes.length == 0) {
-                System.out.println("No matches found with Given Parameters");
+                System.out.println("No matches found with Given Parameters would you like to add an aircraft or try-again with new paramters?: \n 1: Try again \n 2: Add new plane");
+                int userAddOrChooseAnother = Integer.valueOf(scanner.nextLine());
+                if (userAddOrChooseAnother == 2) {
+                    buildPlane();
+                }
             } else {
                 System.out.println("Here is our chosen aircraft based on given parameters: " + PlaneInventory.getRandomPlane(possiblePlanes));
+                System.out.println("Would you like to choose another aircraft or add another aircraft? \n 1: Add aircraft \n 2: Choose another aircraft");
+                int userAddOrChooseAnother = Integer.valueOf(scanner.nextLine());
+                if (userAddOrChooseAnother == 1){
+                    buildPlane();
+                }
             }
         }
+    }
+
+    private static void buildPlane(){
+            System.out.print("What is the name of the aircraft?: ");
+            String newName = scanner.nextLine();
+            System.out.print("What is the range of the aircraft?: ");
+            int newRange = Integer.valueOf(scanner.nextLine());
+            System.out.print("What is the type of aircraft?\n 1: Propellor \n 2: Jet");
+            int newType = Integer.valueOf(scanner.nextLine());
+            System.out.print("What is the speed of da aircraft dawg: ");
+            int newSpeed = Integer.valueOf(scanner.nextLine());
+            inventory.addPlane(newName, newType, newSpeed, newRange);
     }
 }
--- src/PlaneInventory.java
+++ src/PlaneInventory.java
@@ -15,7 +15,7 @@ public class PlaneInventory {
         Plane[] possiblePlanes = new Plane[0];
         for (int i = 0; i < planes.length; i++) {
             if (planes[i].getRange() >= range) {
-                if (planes[i].getType() == type) {
+                if (planes[i].getType() == type || type == 3) {
                     possiblePlanes = Arrays.copyOf(possiblePlanes, possiblePlanes.length+1);
                     possiblePlanes[possiblePlanes.length-1] = planes[i];
                 }
@@ -27,6 +27,11 @@ public class PlaneInventory {
     public static Plane getRandomPlane(Plane[] planes){
         Random randomIndex = new Random();
         return planes[randomIndex.nextInt(planes.length)];
+    }
+
+    public void addPlane(String name, int type, int speed, int range){
+        Arrays.copyOf(planes, planes.length+1);
+        planes[planes.length-1] = new Plane(name, range, type, speed);
     }
 
 
